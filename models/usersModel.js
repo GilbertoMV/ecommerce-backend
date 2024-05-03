288c7eec408eca722652ace7d2a921be13a5a5d3
Gilberto Valenzuela Martinez <54441538+GilbertoMV@users.noreply.github.com>
2024-05-02 23:31:53 -0600

    Update usersModel.js

--- models/usersModel.js
+++ models/usersModel.js
@@ -2,31 +2,31 @@ const { getConnection } = require("../db/db.js");
 
 const getAllUsers = async () => {
     const connection = await getConnection();
-    const [rows] = await connection.execute('SELECT * FROM Usuario');
+    const [rows] = await connection.execute('SELECT * FROM usuario');
     return rows;
 }
 
 const getUserById = async (id) => {
     const connection = await getConnection();
-    const [rows] = await connection.execute('SELECT * FROM Usuario WHERE id_usuario = ?', [id]);
+    const [rows] = await connection.execute('SELECT * FROM usuario WHERE id_usuario = ?', [id]);
     return rows[0];
 }
 
 const createUser = async (userData) => {
     const connection = await getConnection();
-    const [rows] = await connection.query('INSERT INTO Usuario SET ?', userData);
+    const [rows] = await connection.query('INSERT INTO usuario SET ?', userData);
     return rows.insertId;
 };
 
 const deleteUser = async (id) => {
     const connection = await getConnection();
-    const [rows] = await connection.query('DELETE FROM Usuario WHERE id_usuario = ?', [id]);
+    const [rows] = await connection.query('DELETE FROM usuario WHERE id_usuario = ?', [id]);
     return rows.affectedRows;
 }
 
 const updateUser = async (id, userData) => {
     const connection = await getConnection();
-    const [rows] = await connection.query('UPDATE Usuario SET ? WHERE id_usuario = ?', [userData, id]);
+    const [rows] = await connection.query('UPDATE usuario SET ? WHERE id_usuario = ?', [userData, id]);
     return rows.affectedRows;
 }
 
