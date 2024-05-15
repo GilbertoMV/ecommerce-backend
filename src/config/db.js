bc654d1081cf11479a6c139fec49be4e4d9a3dc1
sebas0000 <sgomez4@ucol.mx>
2024-05-15 14:22:13 -0600

    agregar dotenv y buenas practicas 3

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,13 @@
 import mysql from "mysql2/promise"
 import dotenv from 'dotenv'
 dotenv.config('.env')
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.PORT
 });
 
 const getConnection = async () => await connection;
