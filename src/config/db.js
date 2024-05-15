c09e40e6493f7287a703c181ebd3246420f55214
sebas0000 <sgomez4@ucol.mx>
2024-05-15 14:25:36 -0600

    agregar dotenv y buenas practicas 4

--- src/config/db.js
+++ src/config/db.js
@@ -6,8 +6,7 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.PORT
+    password: process.env.DB_PASSWORD
 });
 
 const getConnection = async () => await connection;
