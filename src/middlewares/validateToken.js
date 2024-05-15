8bcd738183d1a8fe1b6b3c749c59e6e3a38074de
GilbertoMV <gvalenzuela@ucol.mx>
2024-05-15 16:18:22 -0600

    endpoint usuarioLogueado

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,25 +1,17 @@
-import jwt from 'jsonwebtoken'
+const jwt = require('jsonwebtoken');
 
-//Nota next es para pasar al siguiente middleware
 const validateToken = (req, res, next) => {
-    //Se requiere que en el encabezado este el authorization y el token, si lo esta lo guarda en const token (el encabezado es parte del front)
-    const token = req.headers['authorization'] ? req.headers['authorization'].split(' ')[1] : null;
-    //Si no hay token se manda un 403 que es Acceso denegado prohibido
-    if (!token) {
-        return res.status(403).json({ message: 'Se requiere token de autenticación' });
-    }
+  const token = req.headers.authorization.split(' ')[1];  // Asumiendo que el token viene como 'Bearer <token>'
 
-    try {
-        //Lo que hace el jwt.verify es agarrar el token y la llave secreta, si el token es valido se decodifica y se guarda en el const 
-        const decodificado = jwt.verify(token, process.env.SECRET_KEY);
-        req.user = decodificado; // Guarda la informacion decodificada en el req.user para usarla luego si es necesaria
-        next(); // Continúa con la siguiente función de middleware
-    } catch (error) {
-        //si el token no es valido se manda un 401
-        res.status(401).json({ message: 'Token inválido o expirado' });
-    }
+  if (!token) {
+    return res.status(403).json({ message: 'Se requiere token de autenticación' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;  // `decoded` ahora contiene el payload del token, incluido el ID del usuario
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Token inválido o expirado' });
+  }
 };
-
-export {
-    validateToken
-}
