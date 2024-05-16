ed5b55afa87e76492520953eeeb0bc2ae0f49250
GilbertoMV <gvalenzuela@ucol.mx>
2024-05-16 11:35:46 -0600

    registro modificado

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -6,17 +6,17 @@ import getUserByEmail from '../models/loginModel.js'
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    const { correo, contrasena, estado_cuenta } = req.body; //Saca el correo y contraseña del POST
+    const {  estado_cuenta,correo, contrasena } = req.body; //Saca el correo y contraseña del POST
     const user = await getUserByEmail(correo); //Busca en la base de datos el correo que se mando en el POST y trae todos los datos de ese correo
+    if(estado_cuenta === '0') {
+        return res.status(401).json({message: 'Cuenta innactiva'})
+    }
     if (!user) { //Si no se encuentra manda error 404
         return res.status(404).json({ message: 'Usuario no encontrado' });
     }
     const isPasswordValid = bcrypt.compareSync(contrasena, user.contrasena); //Compara con el bcrypt la contraseña que se mando en el POST con la de la DB
     if (!isPasswordValid) { //En caso de no ser se manda el status 401 de no autorizado
         return res.status(401).json({ message: 'Contraseña incorrecta' });
-    }
-    if(estado_cuenta === '0') {
-        return res.status(401).json({message: 'Cuenta innactiva'})
     }
     //Si la contraseña esta bien generamos el token con el ID y lo firmamos con la llave secreta, esta puede estar guardada en el railway
     const token = jwt.sign({ id: user.id_usuario }, process.env.SECRET_KEY, { expiresIn: '7d' }); //Tiene expiracion (quiero ver si refrescarla o dejarlo sin tiempo
