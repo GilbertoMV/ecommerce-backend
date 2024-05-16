9ead9d3c115743e8d19058bce51058cfcb40f0c5
GilbertoMV <gvalenzuela@ucol.mx>
2024-05-16 11:06:53 -0600

    registro modificado

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -4,19 +4,21 @@ import buildUserData from '../userData.js'
 
 const router = express.Router();
 //Ruta para crear un usuario y asiganrle un ID
-router.post("/", async (req, res) => {
+router.post("/register", async (req, res) => {
     try {
       //se declara la variable y se llama a la funcion buildUserData y se le pasa hacer la peticion y que debuelva el objeto con los valores
       const userData = buildUserData(req);
       // Pasas userData al modelo para crear el usuario.
       const userId = await createUser(userData);
       // Si todo es exitoso, envías una respuesta con el ID del usuario creado.
-      res.status(201).json({ id: userId, message: "Usuario registrado exitosamente" });
+      res
+        .status(201)
+        .json({ id: userId, message: "Usuario registrado exitosamente" });
     } catch (error) {
       // Si hay un error, se captura y envía una respuesta de error.
       console.error("Error al crear el usuario:", error);
-      if (error.message === "Mail is already registered") {
-        return res.status(400).json({ error: error.message });
+      if (error.code === "ER_DUP_ENTRY") {
+        res.status(400).json({ error: "Email already register" });
       } else {
         res.status(500).json({ error: "Error interno del servidor" });
       }
