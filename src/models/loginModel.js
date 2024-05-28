393464f741c67c1a81912490fc8c0d8e7285258f
sebas0000 <sgomez4@ucol.mx>
2024-05-27 23:13:14 -0600

    Arreglo de errores

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -3,7 +3,7 @@ import getConnection from '../config/db.js'
 
 async function getUserByEmail(email) {
     const connection = await getConnection();
-    const [rows] = await connection.query('SELECT * FROM '+process.env.DB_NAME+'.Usuarios WHERE correo = ?', [email]);
+    const [rows] = await connection.query('SELECT * FROM '+process.env.DB_NAME+'.Usuario WHERE correo = ?', [email]);
     return rows[0];
 }
 export default getUserByEmail;
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -9,14 +9,14 @@ const getAllUsers = async () => {
 
 const getUserById = async (id) => {
     const connection = await getConnection();
-    const [rows] = await connection.execute('SELECT * FROM '+process.env.DB_NAME+'.Usuarios WHERE id_usuario = ?', [id]);
+    const [rows] = await connection.execute('SELECT * FROM '+process.env.DB_NAME+'.Usuario WHERE id_usuario = ?', [id]);
     console.log(id)
     return rows[0];
 }
 
 const deleteUser = async (id) => {
     const connection = await getConnection();
-    const [rows] = await connection.query('DELETE FROM '+process.env.DB_NAME+'.Usuarios WHERE id_usuario = ?', [id]);
+    const [rows] = await connection.query('DELETE FROM '+process.env.DB_NAME+'.Usuario WHERE id_usuario = ?', [id]);
     return rows.affectedRows;
 }
 
@@ -24,7 +24,7 @@ const updateUser = async (id, userData) => {
     const connection = await getConnection();
     const hashContrasena = bcrypt.hashSync(userData.contrasena,10);
     userData.contrasena = hashContrasena;
-    const [rows] = await connection.query('UPDATE '+process.env.DB_NAME+'.Usuarios SET ? WHERE id_usuario = ?', [userData, id]);
+    const [rows] = await connection.query('UPDATE '+process.env.DB_NAME+'.Usuario SET ? WHERE id_usuario = ?', [userData, id]);
     return rows.affectedRows;
 }
 
