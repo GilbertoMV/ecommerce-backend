9238d33495c72f9a706c8696fe95a5114430adad
sebas0000 <sgomez4@ucol.mx>
2024-05-27 22:55:04 -0600

    Arreglando error headers

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const validateToken = (req, res, next) => {
-    const token = req.headers['authorization']
+    const token = req.headers['authorization'] ? req.headers['authorization'].split('')[1] : null;
 
     if (!token) {
         return res.status(403).json({ message: 'Se requiere token de autenticación' });
