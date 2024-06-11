fd18245492cfb6358859a9d0d072cc35f4073ebe
sebas0000 <sgomez4@ucol.mx>
2024-06-10 18:07:55 -0600

    correccion de errores 2

--- src/config/db.js
+++ src/config/db.js
@@ -5,28 +5,13 @@ import dotenv from 'dotenv';
 dotenv.config('.env');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+    
     dialect: 'mysql',
-    logging: false, // Desactivar logs de Sequelize si no los necesitas
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000, // Tiempo de espera antes de lanzar error
-        idle: 10000
-    },
-    dialectOptions: {
-        connectTimeout: 60000 // Aumenta el tiempo de espera de conexión
-    }
+    logging: false,
 });
 
 const connectToDatabase = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Conexión a la base de datos establecida exitosamente.');
-    } catch (error) {
-        console.error('No se pudo conectar a la base de datos:', error);
-        throw error; // Propagar el error para manejarlo fuera de esta función si es necesario
-    }
+    await sequelize.authenticate();
 };
 
 export { connectToDatabase, sequelize };
