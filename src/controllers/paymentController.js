1b58c0c18747b2f4bc3e629a02ae63092ba7ff6d
sebas0000 <sgomez4@ucol.mx>
2024-09-23 11:06:47 -0600

    funcionalidad de pago terminada y probada (funcional)

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,31 +1,36 @@
-import {MercadoPagoConfig, Preference} from 'mercadopago';
+import { MercadoPagoConfig, Preference } from 'mercadopago';
 
-//Creamos el cliete el cual nos mandara lo que debera pagar, el accestoken es de la tienda
-const cliente = new MercadoPagoConfig({
-  access_token: process.env.MPTOKEN
-});
+const client = new MercadoPagoConfig({ 
+    accessToken:process.env.MPTOKEN
+})
 
-//Creamos una ruta donde mandaremos los parametros y variables que nos dan en el front
 export const createPreferences = async (req, res) => {
-    try {
-        const body = {
-            items:[{
-            title: req.body.title,
-            quantity: Number(req.body.quantity),
-            unit_price: Number(req.body.unit_price),
-            currency_id: "MXN"
-        }],
-            back_urls: {
-                //Rutas de donde lo dejaran si damos con exito, error o pendiente el pago, esto no se que hacer
-                success: "http://localhost:3000/success", 
-                failure: "http://localhost:3000/failure",
-                pending: "http://localhost:3000/pending"
-            },
-            auto_return: "approved",
-        };
+  try {
+    const body = {
+      items: [
+        {
+        title: req.body.title,
+        quantity: Number(req.body.quantity),
+        unit_price: Number(req.body.unit_price),
+        currency_id: "MXN"
+      }
+    ],
+      back_urls: {
+        success: "https://youtu.be/TGJY1w88s2Q?si=PY-Y0zRBks_u9HW3",
+        failure: "https://youtu.be/I0ZrvmvIg58?si=bxfcf8R-QZMY_82e",
+        pending: "https://youtu.be/bUmy7Nvsh4s?si=VjipOEgkMxJkFlZZ"
+      },
+      auto_return: "approved"
+    };
 
-        res.status(201).json({id:response.body.id})
-    } catch (error) {
-        res.status(500).json({error: 'Error creando la orden de pago'})
-    }
+    const preference = new Preference(client);
+    const result = await preference.create({ body });
+    const link = `https://www.mercadopago.com.mx/checkout/v1/redirect?pref_id=${result.id}`;
+
+    // Envía el enlace de pago al cliente
+    res.status(201).json({ id: result.id, link });
+  } catch (error) {
+    console.error('Error creando la orden de pago:', error);
+    res.status(500).json({ error: 'Error creando la orden de pago' });
+  }
 };
