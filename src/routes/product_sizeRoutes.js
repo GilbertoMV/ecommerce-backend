bdb9a35484795f4c04ee5b64fb1487ba8033d3bb
sebas0000 <sgomez4@ucol.mx>
2024-10-03 11:42:20 -0600

    Creacion y testeo de talla producto

--- src/routes/product_sizeRoutes.js
+++ src/routes/product_sizeRoutes.js
@@ -12,10 +12,10 @@ const router = express.Router();
 
 router.get('/products', getAllProductSize);
 router.get('/products/:id', getProductSizeByProduct);
-/* TODO: No es necesario por el momento
-router.get('/:id/products/sizes', getProductSizeBySize);*/
 router.post('/products', createProductSize);
-router.put('/:id_talla/products/:id_producto', updateProductSize);
 router.delete('/:id_talla/products/:id_producto', deleteProductSize);
+/* TODO: No es necesario por el momento
+router.get('/:id/products/sizes', getProductSizeBySize);
+router.put('/:id_talla/products/:id_producto', updateProductSize);*/
 
  export default router;
