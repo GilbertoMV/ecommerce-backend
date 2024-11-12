5fd259b0fe43a7e775d7d1384c772ba646d63313
GilbertoVM <gvalenzuela@ucol.mx>
2024-11-12 12:03:55 -0600

    gpt implementado pt5

--- src/controllers/carbon_footprintController.js
+++ src/controllers/carbon_footprintController.js
@@ -3,34 +3,25 @@ import axios from 'axios';
 export const getCarbonFootprint = async (req, res) => {
   try {
     const userMessage = req.body.message;
-    
+    const threadId = process.env.THREAD_ID;
+
     const response = await axios.post(
-      "https://api.openai.com/v1/chat/completions",
+      `https://api.openai.com/v1/threads/${threadId}/messages`,
       {
-        model: "asst_ilgVLDJP6uWasX4uRvXfp5Zw",  // ID de tu asistente personalizado
-        messages: [
-          { role: "user", content: userMessage }  // Solo el mensaje del usuario
-        ],
-        temperature: 0.1,
-        max_tokens: 20,
-        top_p: 1,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
+        content: userMessage,
       },
       {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
         },
-        timeout: 10000  // Límite de tiempo de 10 segundos
       }
     );
 
     const assistantResponse = response.data.choices[0].message.content;
-    
-    res.json({ message: assistantResponse });
+    return res.status(200).send(assistantResponse);
   } catch (error) {
     console.error("Error:", error.response ? error.response.data : error.message);
-    res.status(500).json({ error: "Ocurrió un error al procesar la solicitud" });
+    res.status(500).send("Hubo un error en la respuesta del asistente.");
   }
 };
